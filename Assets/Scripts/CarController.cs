96b0c9d602e430b710b1d7a5905612dd35bfccb5
BUscinski <ben.uscinski@gmail.com>
2014-11-14 02:11:19 -0500

    heres the actual project

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+	public WheelCollider FrontLeftTire;
+	public WheelCollider FrontRightTire;
+	public WheelCollider BackLeftTire;
+	public WheelCollider BackRightTire;
+	
+	public float motorTorque;
+	public float brakeTorque;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.W)){
+			FrontLeftTire.motorTorque = motorTorque;
+			FrontRightTire.motorTorque = motorTorque;	
+		}
+		if(Input.GetKeyUp (KeyCode.W)){
+			FrontLeftTire.motorTorque = 0;
+			FrontRightTire.motorTorque = 0;
+		}
+		if(Input.GetKeyDown (KeyCode.S)){
+			FrontLeftTire.motorTorque = -motorTorque;
+			FrontRightTire.motorTorque = -motorTorque;
+		}
+		if(Input.GetKeyUp (KeyCode.S)){
+			FrontLeftTire.motorTorque = 0;
+			FrontRightTire.motorTorque = 0;
+		}
+		if(Input.GetKey (KeyCode.A)){
+			FrontLeftTire.steerAngle = -20;
+			FrontRightTire.steerAngle = -20;
+		}
+		if(Input.GetKey (KeyCode.D)){
+			FrontLeftTire.steerAngle = 20;
+			FrontRightTire.steerAngle = 20;
+		}
+		if((!Input.GetKey (KeyCode.D)) && (!Input.GetKey (KeyCode.A)))
+		{
+			FrontLeftTire.steerAngle = 0;
+			FrontRightTire.steerAngle = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/ControllerWrapper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerWrapper : MonoBehaviour {
+	public GameObject Cube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire 1"))
+		{
+			Debug.Log ("X");
+		}
+		if(Input.GetButtonDown("Fire 2"))
+		{
+			Debug.Log ("Circle");
+		}
+		if(Input.GetButtonDown ("Fire 3"))
+		{
+			Debug.Log ("Square");
+		}
+		if (Input.GetButtonDown ("Jump"))
+		{
+			Debug.Log ("Triangle");
+		}
+		Cube.rigidbody.AddForce (new Vector3(Input.GetAxis ("LeftStick") * 10, 0, 0));
+	}
+}
