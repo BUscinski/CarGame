5c3cc0f1731b2744a0f840653606577ef8ec9cca
craig <ccerceo@gmail.com>
2014-11-21 16:09:35 -0800

    Explooooode

---
+++ Assets/Scripts/Exploder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exploder : MonoBehaviour 
+{
+	private void Explode()
+	{
+		foreach (Transform t in transform)
+		{
+			t.tag = "Default";
+			var rb = t.gameObject.AddComponent<Rigidbody>();
+			rb.constraints = RigidbodyConstraints.FreezePositionY;
+			rb.AddExplosionForce(500f, t.position, 0f);
+			t.gameObject.AddComponent<SphereCollider>();
+		}
+	}
+}
